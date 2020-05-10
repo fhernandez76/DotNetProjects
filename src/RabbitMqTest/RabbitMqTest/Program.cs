09a661a0d54efbbc013cc47837e76dadcc62cccb
Francisco Hernandez <francisco.hernandez@audatex.com>
2020-05-09 23:56:25 -0400

    Test projects
    Test projects

---
+++ src/RabbitMqTest/RabbitMqTest/Program.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace RabbitMqTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestMethod3();
+        }
+
+        internal static void TestMethod1()
+        {
+            try
+            {
+                ConnectionFactory factory = new ConnectionFactory();
+                factory.Uri = new Uri("amqp://developer:developer@localhost:5672/vh-test");
+
+                Console.WriteLine("Connecting...");
+                IConnection conn = factory.CreateConnection();
+                Console.WriteLine("Connected");
+
+                IModel model = conn.CreateModel();
+
+                var exchangeName = "console-test-exchange";
+                var queueName = "console-test-queue";
+                var consoleTestRoutingKey = "console-test-routing-key";
+
+                model.ExchangeDeclare(exchangeName, ExchangeType.Direct);
+                model.QueueDeclare(queueName, false, false, false, null);
+
+                byte[] messageBodyBytes = Encoding.UTF8.GetBytes("Hello World!");
+                model.BasicPublish(exchangeName, consoleTestRoutingKey, null, messageBodyBytes);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        internal static void TestMethod2()
+        {
+            const string virtualHostName = "vh-test";
+            const string exchangeName = "vhtest-ex-test";
+            const string routingKey = "vhtest-direct-rk";
+            //const string queueName = "vhtestQueue";
+
+            try
+            {
+                var connectionFactory = new ConnectionFactory()
+                {
+                    HostName = "localhost",
+                    UserName = "developer",
+                    Password = "developer",
+                    Port = 5672,
+                    RequestedConnectionTimeout = 3000,
+                    VirtualHost = virtualHostName,
+                };
+
+                using (var rabbitConnection = connectionFactory.CreateConnection())
+                {
+                    using (var channel = rabbitConnection.CreateModel())
+                    {
+                        string body = $"Payload message content";
+                        channel.BasicPublish(exchange: exchangeName, routingKey: routingKey, basicProperties: null, body: Encoding.UTF8.GetBytes(body));
+                        Console.WriteLine("Message sent");
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(ex.ToString());
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+
+            Console.WriteLine("End");
+            Console.Read();
+        }
+
+        internal static void TestMethod3()
+        {
+            const string virtualHostName = "vh-centos-dev";
+            const string exchangeName = "vhcentos-ex-test";
+            const string routingKey = "vh-centos-test-rk";
+            bool exit = false;
+            //const string queueName = "vhtestQueue";
+
+            try
+            {
+                while (!exit)
+                {
+                    Console.WriteLine("Please enter payload message content: ");
+                    string message = Console.ReadLine();
+
+                    var connectionFactory = new ConnectionFactory()
+                    {
+                        HostName = "172.17.60.37",
+                        UserName = "developer",
+                        Password = "developer",
+                        Port = 5672,
+                        RequestedConnectionTimeout = 3000,
+                        VirtualHost = virtualHostName,
+                    };
+
+                    using (var rabbitConnection = connectionFactory.CreateConnection())
+                    {
+                        using (var channel = rabbitConnection.CreateModel())
+                        {
+                            //string body = $"Payload message content";
+                            channel.BasicPublish(exchange: exchangeName, routingKey: routingKey, basicProperties: null, body: Encoding.UTF8.GetBytes(message));
+                            Console.WriteLine("Message sent");
+                        }
+                    }
+
+                    Console.WriteLine("Another message? [Y/N]");
+                    string response = Console.ReadLine();
+                    if (response.ToUpper().Equals("Y")) { exit = false; }
+                    else { exit = true;  }
+                }
+                
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(ex.ToString());
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+
+            Console.WriteLine("End");
+            Console.Read();
+        }
+    }
+}
---
+++ src/TestWorkerServiceRabbit/TestWorkerServiceRabbit/ConsumeRabbitMQHostedService.cs
@@ -0,0 +1,89 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace TestWorkerServiceRabbit
+{
+    public class ConsumeRabbitMQHostedService : BackgroundService
+    {
+        private readonly ILogger _logger;
+        private IConnection _connection;
+        private IModel _channel;
+
+        public ConsumeRabbitMQHostedService(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<ConsumeRabbitMQHostedService>();
+            InitRabbitMQ();
+        }
+
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            stoppingToken.ThrowIfCancellationRequested();
+
+            var consumer = new EventingBasicConsumer(_channel);
+            consumer.Received += (ch, ea) =>
+            {
+                //Received message
+                var content = System.Text.Encoding.UTF8.GetString(ea.Body);
+
+                //Handle the received message
+                HandleMessage(content);
+                _channel.BasicAck(ea.DeliveryTag, false);
+            };
+
+            consumer.Shutdown += OnConsumerShutdown;
+            consumer.Registered += OnConsumerRegistered;
+            consumer.Unregistered += OnConsumerUnregistered;
+            consumer.ConsumerCancelled += OnConsumerConsumerCancelled;
+
+            _channel.BasicConsume("vh-centos-queue-test", autoAck: false, consumer: consumer);
+            return Task.CompletedTask;
+        }
+
+        private void InitRabbitMQ()
+        {
+            var factory = new ConnectionFactory { HostName = "172.17.60.37", 
+                VirtualHost = "vh-centos-dev",
+                UserName = "developer",
+                Password = "developer",
+                Port = 5672
+            };
+
+            //Create connection
+            _connection = factory.CreateConnection();
+
+            //Create channel
+            _channel = _connection.CreateModel();
+
+            System.Collections.Generic.Dictionary<string, object> arguments = new System.Collections.Generic.Dictionary<string, object>();
+            arguments.Add("x-queue-type", "classic");
+
+            _channel.QueueDeclare("vh-centos-queue-test", true, false, false, arguments);
+            _channel.BasicQos(0, 1, false);
+
+            _connection.ConnectionShutdown += RabbitMQ_ConnectionShutdown;
+        }
+
+        private void HandleMessage(string content)
+        {
+            _logger.LogInformation($"consumer received {content}");
+        }
+
+        private void OnConsumerConsumerCancelled(object sender, ConsumerEventArgs e) { }
+        private void OnConsumerUnregistered(object sender, ConsumerEventArgs e) { }
+        private void OnConsumerRegistered(object sender, ConsumerEventArgs e) { }
+        private void OnConsumerShutdown(object sender, ShutdownEventArgs e) { }
+        private void RabbitMQ_ConnectionShutdown(object sender, ShutdownEventArgs e) { }
+
+        public override void Dispose()
+        {
+            _channel.Close();
+            _connection.Close();
+            base.Dispose();
+        }
+
+    }
+}
